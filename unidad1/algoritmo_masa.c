c4e6ebe89eb7a06497a7c8d1b7fbbdaa97623d15
noem20bustan-ui <noem20bustan@gmail.com>
2025-10-30 16:55:00 -0500

    Update algoritmo_masa.c

--- unidad1/algoritmo_masa.c
+++ unidad1/algoritmo_masa.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
 float main() {
-    float masaDelAire, presion, volumen, temperatura;
-
-    printf("Ingrese el valor de la masa:\n ");
-    scanf("%f", &masaDelAire);
-
+        float masaDelAire, presion, volumen, temperatura;
     printf("Ingrese el valor de la presion:\n ");
-    scanf("%f", &presion);
-
+    scanf("%g",&presion);
     printf("Ingrese el valor del volumen:\n ");
-    scanf("%f", &volumen);
-
+    scanf("%g",&volumen);
     printf("Ingrese el valor de la temperatura:\n ");
-    scanf("%f", &temperatura);
-
-    masaDelAire = (presion * volumen) / (0.37 * (temperatura + 460));
-
-    printf("La masa del aire es: %f\n", masaDelAire);
-
+    scanf("%g",&temperatura);
+    masaDelAire=(presion * volumen) / (0.37 * (temperatura + 460));
+    printf("La masa del aire es:%.4g\n",masaDelAire);
     return 0;
 }
 Añadido programa en C para calcular la masa del aire (Unidad 1)
